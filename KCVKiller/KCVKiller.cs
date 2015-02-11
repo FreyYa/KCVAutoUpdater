8f52a96eab029338f50bd419251a9acf57a94058
FreyYa <hong09072@gmail.com>
2015-02-11 15:11:42 +0900

    Message write

--- KCVKiller/KCVKiller.cs
+++ KCVKiller/KCVKiller.cs
@@ -1,13 +1,29 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
+using System.Timers;
 
 namespace KCVKiller
 {
+	/// <summary>
+	/// http://www.csharpstudy.com/Threads/timer.aspx
+	/// </summary>
 	public class KCVKillers
 	{
 		public string processName { get; set; }
+		private bool IsKCVDead { get; set; }
+		private Timer timer = new Timer();
 		public void KCV()
 		{
 			processName = string.Empty;
+
+			
+			timer.Interval = 2000;//E초
+			timer.Elapsed += new ElapsedEventHandler(timer_Elapsed);
+			timer.Start();
+		}
+		private void timer_Elapsed(object sender, ElapsedEventArgs e)
+		{
+			Console.WriteLine("제독업무도 바빠!의 종료를 확인중입니다...");
 			foreach (Process process in Process.GetProcesses())
 			{
 				if (process.ProcessName.StartsWith("KanColleViewer") || process.ProcessName.StartsWith("KanColleViewer-Horizontal"))
@@ -15,10 +31,20 @@ namespace KCVKiller
 					if (!process.ProcessName.Contains("vshost"))
 					{
 						processName = process.ProcessName;
-						process.Kill();
 					}
 				}
+				else
+				{
+					IsKCVDead = true;
+				}
+				if (IsKCVDead)
+				{
+					timer.Stop();
+					timer.Dispose();
+					timer.Close();
+				}
 			}
+			
 		}
 	}
 }
